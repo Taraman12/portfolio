840af06fc446c7afb4f4e3b7c8e8b53791efc3ef
Taraman12 <Fabian.Kochsiek@gmail.com>
2025-08-26 23:19:33 +0200

    feat: added SectionDevider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+import SectionDivider from "@/components/SectionDivider";
+
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Hero />
+        <SectionDivider>Techstack</SectionDivider>
         <Skills />
       </main>
     </div>
