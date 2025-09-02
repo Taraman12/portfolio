dfa3bf5bd56944d4c6dc5e6c097bb75665e7f895
Fabian <Fabian.Kochsiek@gmail.com>
2025-09-02 17:05:11 +0200

    feat: removed the top padding from the Experience Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex w-full max-w-5xl flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Hero />
-        <SectionDivider>Experience</SectionDivider>
+        <SectionDivider className="pt-0">Experience</SectionDivider>
         <Experience />
         <SectionDivider>Tech Stack</SectionDivider>
         <Skills />
