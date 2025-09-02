87d91d45394db8dfebc3a5b5cf0d1d2417d86c1d
Fabian <Fabian.Kochsiek@gmail.com>
2025-09-02 16:38:57 +0200

    feat: added pt-8 to devider

--- src/components/SectionDivider.tsx
+++ src/components/SectionDivider.tsx
@@ -6,7 +6,7 @@ interface DividerProps {
 
 const SectionDivider: React.FC<DividerProps> = ({ children }) => {
     return (
-        <h2 className="w-full text-center text-xl font-extrabold tracking-tight sm:text-2xl lg:text-3xl">
+        <h2 className="w-full text-center text-xl font-extrabold tracking-tight sm:text-2xl lg:text-3xl pt-8">
             {children}
         </h2>
     );
