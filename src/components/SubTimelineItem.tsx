74242ae0ca87d390e6953f5fb861ec8799a249c3
Fabian <Fabian.Kochsiek@gmail.com>
2025-09-03 22:44:37 +0200

    fix: fixed icon background

--- src/components/SubTimelineItem.tsx
+++ src/components/SubTimelineItem.tsx
@@ -16,13 +16,13 @@ export default function SubTimelineItem({ experience }: Props) {
             <Link
                 href={href}
                 target="_blank"
-                className="absolute -left-16 top-4 flex items-center justify-center rounded-full bg-white"
+                className="absolute -left-16 top-4 flex items-center justify-center rounded-full dark:bg-neutral-800"
             >
                 <Avatar className="size-12 border">
                     <AvatarImage
                         src={`${prefix}${logo}`}
                         alt={name}
-                        className="bg-background object-contain"
+                        className="object-contain"
                     />
                     <AvatarFallback>{name[0]}</AvatarFallback>
                 </Avatar>
--- src/components/TimelineItem.tsx
+++ src/components/TimelineItem.tsx
@@ -19,13 +19,13 @@ export default function TimelineItem({ experience }: Props) {
             <Link
                 href={href}
                 target="_blank"
-                className="absolute -left-16 top-4 flex items-center justify-center rounded-full bg-white"
+                className="absolute -left-16 top-4 flex items-center justify-center rounded-full bg-gray-50 dark:bg-neutral-800"
             >
                 <Avatar className="size-12 border">
                     <AvatarImage
                         src={`${prefix}${logo}`}
                         alt={name}
-                        className="bg-background object-contain"
+                        className="object-contain"
                     />
                     <AvatarFallback>{name[0]}</AvatarFallback>
                 </Avatar>
